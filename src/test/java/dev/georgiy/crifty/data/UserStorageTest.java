9e49ba51c25279085ac8e717bf4fdca881dda7c3
GiorgosDev <mayshmaz@gmail.com>
2020-02-08 21:00:29 +0200

    added tests sceleton

---
+++ src/test/java/dev/georgiy/crifty/data/UserStorageTest.java
@@ -0,0 +1,20 @@
+package dev.georgiy.crifty.data;
+
+import org.junit.Test;
+
+public class UserStorageTest {
+    @Test
+    public void createUser(){
+        //todo
+    }
+
+    @Test
+    public void deleteUser(){
+        //todo
+    }
+
+    @Test
+    public void verifyUser(String userId, String token){
+        //todo
+    }
+}
