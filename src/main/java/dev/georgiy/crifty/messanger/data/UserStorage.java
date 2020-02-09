3ecb1f557080ded24e4f6849bf86ba3431c5901f
GiorgosDev <mayshmaz@gmail.com>
2020-02-09 13:15:21 +0200

    verify token method added

--- src/main/java/dev/georgiy/crifty/messanger/data/UserStorage.java
+++ src/main/java/dev/georgiy/crifty/messanger/data/UserStorage.java
@@ -4,5 +4,7 @@ import dev.georgiy.crifty.messanger.data.beans.User;
 
 public interface UserStorage {
     void addUser(User user);
+    boolean isRegistered(String userId);
     void deleteUser(String userId);
+    boolean isRegistered(String userId, String token);
 }
