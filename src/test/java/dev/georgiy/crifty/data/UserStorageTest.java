92aa00aa3cf19c84e6d90c0c1219e90b7be9bf57
GiorgosDev <mayshmaz@gmail.com>
2020-02-09 13:26:59 +0200

    tests for user storage created

--- src/test/java/dev/georgiy/crifty/data/UserStorageTest.java
+++ src/test/java/dev/georgiy/crifty/data/UserStorageTest.java
@@ -1,20 +1,37 @@
 package dev.georgiy.crifty.data;
 
-import org.junit.Test;
+import dev.georgiy.crifty.messanger.data.UserStorage;
+import dev.georgiy.crifty.messanger.data.beans.User;
+import dev.georgiy.crifty.messanger.data.inmemory.UserStorageImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
 
 public class UserStorageTest {
     @Test
-    public void createUser(){
-        //todo
+    public void createUserTest(){
+        UserStorage userStorage = new UserStorageImpl();
+        User user = new User();
+        String id = user.getId();
+        userStorage.addUser(user);
+        Assert.isTrue( userStorage.isRegistered(id), "The user must be found");
     }
 
     @Test
     public void deleteUser(){
-        //todo
+        UserStorage userStorage = new UserStorageImpl();
+        User user = new User();
+        String id = user.getId();
+        userStorage.addUser(user);
+        userStorage.deleteUser(id);
+        Assert.isTrue( userStorage.isRegistered(id), "The user must be deleted");
     }
 
     @Test
-    public void verifyUser(String userId, String token){
-        //todo
+    public void verifyUser(){
+        UserStorage userStorage = new UserStorageImpl();
+        User user = new User();
+        String id = user.getId();
+        String token = userStorage.addUser(user);
+        userStorage.isRegistered(id, token);
     }
 }
