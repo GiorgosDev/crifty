604b0a17e044db7d822e8be827894143005e8c8c
GiorgosDev <mayshmaz@gmail.com>
2020-02-09 13:25:56 +0200

    token functionality added

--- src/main/java/dev/georgiy/crifty/messanger/data/UserStorage.java
+++ src/main/java/dev/georgiy/crifty/messanger/data/UserStorage.java
@@ -3,7 +3,7 @@ package dev.georgiy.crifty.messanger.data;
 import dev.georgiy.crifty.messanger.data.beans.User;
 
 public interface UserStorage {
-    void addUser(User user);
+    String addUser(User user);
     boolean isRegistered(String userId);
     void deleteUser(String userId);
     boolean isRegistered(String userId, String token);
