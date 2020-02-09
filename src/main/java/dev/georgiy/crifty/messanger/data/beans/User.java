391ac1fb81962464571476897dc90fa8ec81701c
GiorgosDev <mayshmaz@gmail.com>
2020-02-09 12:47:59 +0200

    user bean implemented

--- src/main/java/dev/georgiy/crifty/messanger/data/beans/User.java
+++ src/main/java/dev/georgiy/crifty/messanger/data/beans/User.java
@@ -1,11 +1,28 @@
 package dev.georgiy.crifty.messanger.data.beans;
 
+import java.util.Objects;
+import java.util.UUID;
+
 public class User {
+    private String id;
     public User() {
+        this.id = UUID.randomUUID().toString();
     }
 
     public User(String userId) {
+        this.id = userId;
     }
 
-    //todo equals, hashcode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
