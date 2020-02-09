513738d3c56c022d030e7910ba51162f15253bc3
GiorgosDev <mayshmaz@gmail.com>
2020-02-09 13:25:28 +0200

    token functionality added

--- src/main/java/dev/georgiy/crifty/messanger/data/inmemory/UserBucket.java
+++ src/main/java/dev/georgiy/crifty/messanger/data/inmemory/UserBucket.java
@@ -2,10 +2,23 @@ package dev.georgiy.crifty.messanger.data.inmemory;
 
 import dev.georgiy.crifty.messanger.data.beans.User;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class UserBucket {
     private Map<User, MessageBucket> buckets = new ConcurrentHashMap<>();
+    private Set<String> tokens = ConcurrentHashMap.newKeySet();
 
+    public String createToken(){
+        String token = UUID.randomUUID().toString();
+        tokens.add(token);
+        return token;
+    }
+
+    public boolean isValidToken(String token){
+        return tokens.contains(token);
+    }
 }
