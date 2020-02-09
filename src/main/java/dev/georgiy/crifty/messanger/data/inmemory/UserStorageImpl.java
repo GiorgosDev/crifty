6c10564f6eace3112b5773c8ed9fcce003804cb3
GiorgosDev <mayshmaz@gmail.com>
2020-02-09 13:26:37 +0200

    user authentication added

--- src/main/java/dev/georgiy/crifty/messanger/data/inmemory/UserStorageImpl.java
+++ src/main/java/dev/georgiy/crifty/messanger/data/inmemory/UserStorageImpl.java
@@ -10,12 +10,28 @@ public class UserStorageImpl implements UserStorage {
     private Map<User, UserBucket> buckets = new ConcurrentHashMap<>();
 
     @Override
-    public void addUser(User user) {
-        buckets.put(user, new UserBucket());
+    public String addUser(User user) {
+        UserBucket bucket = new UserBucket();
+        buckets.put(user, bucket);
+        String token = bucket.createToken();
+        return token;
     }
+
+    @Override
+    public boolean isRegistered(String id) {
+        User user = new User(id);
+        return buckets.containsKey(user);
+    }
+
 
     @Override
     public void deleteUser(String userId) {
         buckets.put(new User(userId), new UserBucket());
+    }
+
+    @Override
+    public boolean isRegistered(String userId, String token) {
+        UserBucket bucket = buckets.get(new User(userId));
+        return bucket!=null && bucket.isValidToken(token);
     }
 }
