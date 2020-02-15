a69f41e8cc466fef9253a4f61902c7477f2c2f28
GiorgosDev <mayshmaz@gmail.com>
2020-02-15 22:20:13 +0200

    tests for message storage created

---
+++ src/test/java/dev/georgiy/crifty/data/MessageStorageTest.java
@@ -0,0 +1,15 @@
+package dev.georgiy.crifty.data;
+
+import org.junit.jupiter.api.Test;
+
+public class MessageStorageTest {
+    @Test
+    public void addMessageTest() {
+
+    }
+
+    @Test
+    public void getMessagesTest() {
+
+    }
+}
