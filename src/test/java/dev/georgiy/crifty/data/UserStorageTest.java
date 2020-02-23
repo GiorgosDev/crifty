4f1fb4c40c7bc44865be0d77a3d47465e33f5577
GiorgosDev <mayshmaz@gmail.com>
2020-02-23 20:39:19 +0200

    test fixed to use junit

--- src/test/java/dev/georgiy/crifty/data/UserStorageTest.java
+++ src/test/java/dev/georgiy/crifty/data/UserStorageTest.java
@@ -3,8 +3,9 @@ package dev.georgiy.crifty.data;
 import dev.georgiy.crifty.messanger.data.UserStorage;
 import dev.georgiy.crifty.messanger.data.beans.User;
 import dev.georgiy.crifty.messanger.data.inmemory.UserStorageImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.util.Assert;
+
 
 public class UserStorageTest {
     @Test
@@ -13,7 +14,7 @@ public class UserStorageTest {
         User user = new User();
         String id = user.getId();
         userStorage.addUser(user);
-        Assert.isTrue( userStorage.isRegistered(id), "The user must be found");
+        Assertions.assertTrue( userStorage.isRegistered(id), "The user must be found");
     }
 
     @Test
@@ -23,7 +24,7 @@ public class UserStorageTest {
         String id = user.getId();
         userStorage.addUser(user);
         userStorage.deleteUser(id);
-        Assert.isTrue( userStorage.isRegistered(id), "The user must be deleted");
+        Assertions.assertTrue( userStorage.isRegistered(id), "The user must be deleted");
     }
 
     @Test
